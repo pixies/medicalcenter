4c659c29feff5c119f665cb5efefc392e90479cc
Cleisson Fabricio <cleissonf@gmail.com>
2018-07-02 07:39:47 -0300

    ultimas modificacoes no template

--- conta/views.py
+++ conta/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def atendente(request):
+     return render(request,"atendente.html",{})  
+
+
+def medico(request):
+     return render(request,"medico.html",{})  
