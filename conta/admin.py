fd2ab0550aaa7b147646f5c32ef0c2bd924f2669
Cleisson Fabricio <cleissonf@gmail.com>
2018-07-02 07:38:02 -0300

    conta admin

--- conta/admin.py
+++ conta/admin.py
@@ -1,8 +1,2 @@
 from django.contrib import admin
-from conta.models import Profile
-# Register your models here.
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    model = Profile
 
